3f8ee7f5553462042723529717ae2fad1c6d385c
tskim <tskim@carby.co.kr>
2020-02-18 07:05:27 +0900

    Init Comiit

---
+++ TDDCalculate/TDDCalculate/TDDCalculate.h
@@ -0,0 +1,19 @@
+//
+//  TDDCalculate.h
+//  TDDCalculate
+//
+//  Created by Tyler on 2020/02/18.
+//  Copyright © 2020 Assin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TDDCalculate.
+FOUNDATION_EXPORT double TDDCalculateVersionNumber;
+
+//! Project version string for TDDCalculate.
+FOUNDATION_EXPORT const unsigned char TDDCalculateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDDCalculate/PublicHeader.h>
+
+
